f7bc86a9d36d676e64d01408cf4f9d6fb3ff7d8f
Sigularusrex <djsigley@gmail.com>
2017-05-11 08:50:55 +0100

    setup config file

--- lib/generators/templates/geolocation.rb
+++ lib/generators/templates/geolocation.rb
@@ -1,19 +1,5 @@
-class Geolocation < ActiveRecord::Migration
-  def self.up
-    create_table :geolocations do |t|
-      t.inet :ip_address
-      t.string :country_code
-      t.string :country
-      t.string :city
-      t.float :latitude
-      t.float :longitude
-      t.integer :mystery_value, limit: 8
-    end
-    add_index :geolocations, :ip_address, unique: true
+Geolocation.configure do |config|
 
-  end
-
-  def self.down
-    drop_table :geolocations
-  end
+  #Sets number of rows to download. Set Limit to 0 to download full file
+  config.limit = 1
 end
