f68bfb0b3e3181c193e31ae40b88e4b0581c132f
Sigularusrex <djsigley@gmail.com>
2017-05-11 08:51:36 +0100

    configuration

--- lib/geolocation.rb
+++ lib/geolocation.rb
@@ -1,7 +1,22 @@
 require "geolocation/version"
 require "models/location"
 
-
 module Geolocation
   # Your code goes here...
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield configuration
+  end
+
+
+  class Configuration
+    include ActiveSupport::Configurable
+
+    config_accessor(:limit) { 0 }
+    config_accessor(:file_name) # no default (nil)
+  end
+
 end
