8bf0bada63ea6b23166716951c1cb0f82eb4393b
Sigularusrex <djsigley@gmail.com>
2017-05-11 08:52:14 +0100

    fetch data script

---
+++ lib/generators/geolocation/fetch_data/fetch_data_generator.rb
@@ -0,0 +1,51 @@
+require 'rails/generators/migration'
+
+module Geolocation
+  module Generators
+    class FetchDataGenerator < ::Rails::Generators::Base
+      desc "Populate the Locations Table"
+
+      require 'csv'
+
+      #Delete table before populating, for testing
+      Geolocation::Location.delete_all
+
+      # setup stats variables
+      errors = []
+      rejected = 0
+      processed = 0
+      start_time = Time.now
+
+      begin
+      file_name = Geolocation.configuration.file_name
+      csv_text = File.read(file_name)
+      csv = CSV.parse(csv_text, :headers => true)
+
+      # Process CSV
+      csv.each do |row|
+        begin
+          Geolocation::Location.create!(row.to_hash)
+        rescue ActiveRecord::RecordInvalid => e
+          rejected += 1
+          errors.push({
+                          messages: e.record.errors.messages
+                      })
+        end
+          processed += 1
+        if processed == Geolocation.configuration.limit then break end
+      end
+
+      rescue Errno::ENOENT
+        puts "Error: CSV file not found or corrupted"
+      end
+
+      # Display Stats
+      end_time = Time.now
+      elapsed_time = (end_time - start_time) * 100.0
+      accepted = processed - rejected
+      puts "Accepted Records #{accepted}"
+      puts "Rejected Records #{rejected}"
+      puts "Elapsed Time #{elapsed_time} Seconds"
+    end
+  end
+end
